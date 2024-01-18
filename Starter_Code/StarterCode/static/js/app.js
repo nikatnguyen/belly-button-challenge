f69c32e51ebf90d28b8ab1b4e330d4cca61ddb9e
nikatnguyen <anguyen11@seattleu.edu>
2024-01-18 11:24:49 -0800

    startover

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -1,58 +1,23 @@
+
+
 // Use the D3 library to read in samples.json from the URL https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.
 let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
 d3.json(url).then(function(data) {
     console.log(data);
   });
+let samples = data.map(function(data){
+    return data.metadata
+}):
+
 
 //Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
-function topTen(data) {
-    // return player.madeTeam == true;
-    // A more concise way to express a boolean conditional
-    return player.madeTeam;
-  }
 
-function init() {
-    let data = [{
-      values: australia,
-      labels: labels,
-      type: "bar"
-    }];
-  
-    let layout = {
-      height: 600,
-      width: 800
-    };
-  
-    Plotly.newPlot("bar", data, layout);
-  }
-function getData() {
-    let dropdownMenu = d3.select("#selDataset");
     // Assign the value of the dropdown menu option to a letiable
-    let dataset = dropdownMenu.property("value");
-    // Initialize an empty array for the country's data
-    let data = [];
   
-    if (dataset == 'australia') {
-        data = australia;
-    }
-    else if (dataset == 'brazil') {
-        data = brazil;
-    }
-    else if (dataset == 'uk') {
-        data = uk;
-    }
-    else if (dataset == 'mexico') {
-      data = mexico;
-    }
-    else if (dataset == 'singapore') {
-        data = singapore;
-    }
-    else if (dataset == 'southAfrica') {
-      data = southAfrica;
-    }
-
+    // Initialize an empty array for the country's data
+   
 ////Use sample_values as the values for the bar chart.
 
 ////Use otu_ids as the labels for the bar chart.
@@ -81,5 +46,5 @@ function getData() {
 //Update all the plots when a new sample is selected. Additionally, you are welcome to create any layout that you would like for your dashboard.
 
 
-Deploy your app to a free static page hosting service, such as GitHub Pages. Submit the links to your deployment and your GitHub repo. Ensure that your repository has regular commits and a thorough README.md file
+//Deploy your app to a free static page hosting service, such as GitHub Pages. Submit the links to your deployment and your GitHub repo. Ensure that your repository has regular commits and a thorough README.md file
 
---
+++ Starter_Code/StarterCode/static/js/new.js
@@ -0,0 +1,10 @@
+// Use the D3 library to read in samples.json from the URL https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.
+let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+d3.json(url).then(function(data) {
+    console.log(data);
+  });
+let samples = data.map(function(all){
+    return all.metadata
+}):
+
